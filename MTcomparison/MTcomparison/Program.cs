8ef96e3006d13abaabb5420a1057085e772d4ea3
CARICOM12 <0190a@ukr.net>
2018-05-14 20:25:55 +0300

    "Welcome message was added"

---
+++ MTcomparison/MTcomparison/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTcomparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome, it's a project which compares different machine translation services");
+            Console.ReadLine();
+        }
+    }
+}
